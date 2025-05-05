e0fb35371df044c2b0d674993b3abab381fad089
vinothgopi <wearestartsup@hotmail.com>
2025-05-05 18:33:39 +0530

    java file modified

--- Test.java
+++ Test.java
@@ -2,6 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World from Java!");
         System.out.println("This is a test file!");        
-        System.out.println("This is a test file!");
     }
 }
